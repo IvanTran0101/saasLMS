a647d43642bb3ac078771b704b265000b6bfbb0b
VuThanh2 <chauthanhvu24122007@gmail.com>
2026-04-05 15:31:16 +0700

    update Eto for LearningProgress Service

--- services/learningProgress/src/saasLMS.LearningProgressService.Domain.Shared/Etos/LessonProgresses/LastLearningPositionUpdatedEto.cs
+++ services/learningProgress/src/saasLMS.LearningProgressService.Domain.Shared/Etos/LessonProgresses/LastLearningPositionUpdatedEto.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using saasLMS.LearningProgressService.LessonProgresses;
+using Volo.Abp.EventBus;
 
 namespace saasLMS.LearningProgressService.Etos.LessonProgresses;
 
+[EventName("lms.learning_progress.last_learning_position_updated.v1")]
 public class LastLearningPositionUpdatedEto
 {
     public Guid                 EventId    { get; init; }
